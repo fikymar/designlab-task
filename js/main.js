2548806fea85ab1c3858c4667adbc6548eb6c8fe
fikymar <marcela@creativehandles.com>
2023-03-31 17:57:11 +0200

    gulp, sass added

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ function setActiveLink() {
 	navLinks.forEach((link) => {
 		// console.log(link.href, window.location.href)
 		if (link.href === window.location.href) {
-			link.setAttribute('aria-current', 'page')
-		}
+			link.classList.add('bg-primary')
+		} else link.classList.remove('bg-primary')
 	})
 }
 setActiveLink()
