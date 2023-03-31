22ca9262197c7e76638660e7ff464638deb08efb
fikymar <marcela@creativehandles.com>
2023-03-31 17:59:16 +0200

    remove log

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 let navLinks = document.querySelectorAll('.nav-link')
 function setActiveLink() {
 	navLinks.forEach((link) => {
-		// console.log(link.href, window.location.href)
 		if (link.href === window.location.href) {
 			link.classList.add('bg-primary')
 		} else link.classList.remove('bg-primary')
